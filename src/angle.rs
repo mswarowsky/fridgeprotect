da423601d735d59dc2436f24f5784dc461df9c58
Markus Swarowsky <markus.swarowsky@gmail.com>
2023-07-08 14:45:40 +0200

    Own angle module

---
+++ src/angle.rs
@@ -0,0 +1,69 @@
+use micromath::F32Ext;
+
+use microbit::{
+    hal::twim,
+    pac::TWIM0,
+};
+
+use lsm303agr::{
+    interface::I2cInterface, mode::MagOneShot, AccelMode, AccelOutputDataRate, Lsm303agr,
+};
+
+type AccSensor = Lsm303agr<I2cInterface<twim::Twim<TWIM0>>, MagOneShot>;
+
+const PI : f32 = 3.14159265359;
+const LSM303AGR_SENSITIVITY : i32 = 988;
+const DAMPING_FACTOR : f32 = 0.05;
+
+pub struct Angle {
+sensor: AccSensor,
+x_damped : f32
+}
+
+
+fn arc2degree(arc : f32) -> f32 {
+return (arc/PI)*180.0
+}
+
+fn limit(a : f32, limit: f32) -> f32 {
+if a > limit {
+    return limit;
+} else {
+    return a;
+}
+}
+
+impl Angle {
+pub fn new(mut acc_sensor : AccSensor) -> Self {
+    match acc_sensor.accelerometer_id() {
+        Ok(0x33u8) => {}
+        _ => (),
+        // _ => defmt::panic!("accelerometer not found"),
+    }
+    acc_sensor.init().unwrap();
+    acc_sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    acc_sensor.set_accel_mode(AccelMode::HighResolution).unwrap();
+    Self { sensor: acc_sensor,
+        x_damped : 0.0 }
+}
+    
+    /* X sensor is on horizontal line, so at 0 zero force is applied to it 
+    * so the arcsine calculates the ang
+    struct Angle {el 
+        */
+pub fn get_current_angle(&self) -> i32{
+    let mut acc = self.x_damped / LSM303AGR_SENSITIVITY as f32; 
+    acc = limit(acc, 1.0);
+    return arc2degree(acc.asin()) as i32;
+}
+        
+pub fn update(&mut self) {
+    loop {
+        if self.sensor.accel_status().unwrap().xyz_new_data {
+            let x_raw = self.sensor.accel_data().unwrap().x as f32;
+            self.x_damped = self.x_damped * (1.0 - DAMPING_FACTOR) + x_raw.abs() * DAMPING_FACTOR;
+            return;
+        }
+    }
+}
+}
--- src/main.rs
+++ src/main.rs
@@ -9,74 +9,22 @@ use rtt_target::rprintln;
 use cortex_m_rt::entry;
 
 use microbit::hal::Timer;
-use micromath::F32Ext;
 
 
 use microbit::{
     hal::twim,
     display::blocking::Display,
-    pac::{twim0::frequency::FREQUENCY_A, TWIM0},
+    pac::{twim0::frequency::FREQUENCY_A},
 };
 
-/*-------------------------------------------- */ 
-use lsm303agr::{
-    interface::I2cInterface, mode::MagOneShot, AccelMode, AccelOutputDataRate, Lsm303agr,
-};
+use lsm303agr::Lsm303agr;
 
-type Sensor = Lsm303agr<I2cInterface<twim::Twim<TWIM0>>, MagOneShot>;
+/* ----------------- Angle --------------- */
+pub mod angle;
+use crate::angle::Angle;
 
-const PI : f32 = 3.14159265359;
-const LSM303AGR_SENSITIVITY : i32 = 988;
-const DAMPING_FACTOR : f32 = 0.05;
 const ANGLE_LIMIT : i32 = 30;
-
-struct Angle {
-    x_damped : f32
-}
-
-
-fn arc2degree(arc : f32) -> f32 {
-    return (arc/PI)*180.0
-}
-
-fn limit(a : f32, limit: f32) -> f32 {
-    if a > limit {
-        return limit;
-    } else {
-        return a;
-    }
-}
-
-impl Angle {
-
-    pub fn new() -> Self {
-        Self { x_damped : 0.0 }
-    }
-
-    /* X sensor is on horizontal line, so at 0 zero force is applied to it 
-     * so the arcsine calculates the ang
-struct Angle {el 
-     */
-    fn get_current_angle(&self) -> i32{
-        let mut acc = self.x_damped / LSM303AGR_SENSITIVITY as f32; 
-        acc = limit(acc, 1.0);
-        return arc2degree(acc.asin()) as i32;
-    }
-
-    fn update(&mut self, sensor: &mut Sensor) {
-        loop {
-            if sensor.accel_status().unwrap().xyz_new_data {
-                let x_raw = sensor.accel_data().unwrap().x as f32;
-                self.x_damped = self.x_damped * (1.0 - DAMPING_FACTOR) + x_raw.abs() * DAMPING_FACTOR;
-                return;
-            }
-        }
-
-    }
-}
-/*-------------------------------------------------*/
-
-
+/* ----------------- Angle --------------- */
 
 const DISP_OK : [[u8;5]; 5]= [
     [0, 0, 0, 1, 0],
@@ -84,13 +32,13 @@ const DISP_OK : [[u8;5]; 5]= [
     [0, 1, 1, 0, 0],
     [0, 1, 0, 0, 0],
     [1, 0, 0, 0, 0],
-];
-
-
+    ];
+    
+    
 const DISP_FAIL : [[u8;5]; 5]= [
     [1, 0, 0, 0, 1],
     [0, 1, 0, 1, 0],
-    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0],
     [0, 1, 0, 1, 0],
     [1, 0, 0, 0, 1],
 ];
@@ -102,19 +50,11 @@ fn main() -> ! {
     let mut timer = Timer::new(board.TIMER0);
 
     
-    let mut angel : Angle = Angle::new();
-    /* ---- ANGLE ----------------- */
+    /* ----------------- Angle --------------- */
     let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
-    let mut acc_sensor = Lsm303agr::new_with_i2c(i2c);
-    match acc_sensor.accelerometer_id() {
-        Ok(0x33u8) => {}
-        _ => (),
-        // _ => defmt::panic!("accelerometer not found"),
-    }
-    acc_sensor.init().unwrap();
-    acc_sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
-    acc_sensor.set_accel_mode(AccelMode::HighResolution).unwrap();
-    /* ---- ANGLE ----------------- */
+    let acc_sensor = Lsm303agr::new_with_i2c(i2c);
+    let mut angel : Angle = Angle::new(acc_sensor);
+    /* ----------------- Angle --------------- */
 
     /* ---------------- Display -------------- */
     let mut display = Display::new(board.display_pins);
@@ -125,8 +65,7 @@ fn main() -> ! {
     
     
     loop {
-        /* ---- ANGLE ----------------- */
-        angel.update(&mut acc_sensor);
+        angel.update();
         let degrees = angel.get_current_angle();
         rprintln!("Current Angle: {}°", degrees);
         if degrees > ANGLE_LIMIT {
